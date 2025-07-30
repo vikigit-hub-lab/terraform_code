216346098e2e1e5b06ee7e23870a77fd18eefd6e
venkateswarareddym06 <venkateswarareddym06@gmail.com>
2025-07-30 19:51:04 +0530

    eventhubs.tf

---
+++ Eventhubs.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_eventhub_namespace" "example" {
+  name                = "example-namespace"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku                 = "Standard"
+  capacity            = 2
+}
+
+resource "azurerm_eventhub" "example" {
+  name                = "example-eventhub"
+  namespace_name      = azurerm_eventhub_namespace.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  partition_count     = 2
+  message_retention   = 1
+}
+
+resource "azurerm_eventhub_consumer_group" "example" {
+  name                = "example-consumergroup"
+  namespace_name      = azurerm_eventhub_namespace.example.name
+  eventhub_name       = azurerm_eventhub.example.name
+  resource_group_name = azurerm_resource_group.example.name
+}
